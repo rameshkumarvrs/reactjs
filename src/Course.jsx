87b0cdb30ccb61bd013dd45e54f43a50931ec257
ramesh <rameshkumarvrs@gmail.com>
2026-06-25 09:05:06 +0530

    added custom css for the components

--- src/Course.jsx
+++ src/Course.jsx
@@ -2,7 +2,7 @@ const course = "Reactjs"
 
 function Course() {
     return(
-        <div>
+        <div className="card">
             <img src="" alt="" />
             <h3>{course}</h3>
             <p>This is {2+5} course</p>
