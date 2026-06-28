290225323201a7e072ad97b696a8050bf58b1b67
ramesh <rameshkumarvrs@gmail.com>
2026-06-28 23:59:04 +0530

    added the bootstrap login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -5,21 +5,25 @@ const Login = () => {
     <>
     
 
-    <form>
+    <form className="my-5" style={{width:"50%", margin:"auto"}}>
         <div class="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            <label  className="form-label">Email address</label>
+            <input type="email" className="form-control"  />
+            
         </div>
         <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" />
+            <label  class="form-label">Password</label>
+            <input type="password" className="form-control" />
+        </div>
+        <div class="mb-3">
+            <label  class="form-label">Re-enter-Password</label>
+            <input type="password" className="form-control" />
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-            <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            <label className="form-check-label" >I agree</label>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">Create Account</button>
 </form>
 
 
