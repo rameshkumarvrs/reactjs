16f30931b7190d874a2aa103a98b8c00b5e17838
ramesh <rameshkumarvrs@gmail.com>
2026-06-29 19:29:36 +0530

    add create router dome with element and path attribute and router provider with routing

--- src/index.js
+++ src/index.js
@@ -4,12 +4,27 @@ import './index.css';
 import App from './App';
 import Login from './Login';
 import reportWebVitals from './reportWebVitals';
+import Home from './Home';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom' 
+
+const router = createBrowserRouter([
+
+  {
+    path : "/",
+    element : <Home />
+  }, 
+  {
+    path : "/login",
+    element : <Login />
+  }
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
-    <Login />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
